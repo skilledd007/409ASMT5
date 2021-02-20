b9d4fb01373a3c2e12826d7a61a46b910dffce11
skilledd007 <65367463+skilledd007@users.noreply.github.com>
2021-02-19 21:01:37 -0700

    Got most of it done, just need to finish Author, series and Contract

--- MyBook.java
+++ MyBook.java
@@ -2,11 +2,291 @@ public class MyBook{
 
 }
 class Book {
+    private String isbn;
+    private int publicationYear;
+    private int pages;
+    
+    public Book()
+    {
+
+    }
+
+    public Book(String isbn, int pages)
+    {
+        this.isbn = isbn;
+        this.pages = pages;
+    }
+    public void setIsbn(String isbn)
+    {
+        this.isbn = isbn;
+    }
+    public String getIsbn()
+    {
+        return this.isbn;
+    }
+    public void setPublicationYear(int publicationYear)
+    {
+        this.publicationYear = publicationYear;
+    }
+    public int getPublicationYear()
+    {
+        return this.publicationYear;
+    }
+    public void setPages(int pages)
+    {
+        this.pages = pages;
+    }
+    public int getPages()
+    {
+        return this.pages;
+    }
 
 }
 class Hardcover extends Book{
 
+    public Hardcover()
+    {
+        super();
+    }
+
+    public String binding()
+    {
+        return "Method binding called from Hardcover";
+    }
+
 }
 class Paperback extends Book{
-    
+    public Paperback()
+    {
+        super();
+    }
+    public String coverArt()
+    {
+        return "Method coverArt called from Paperback";
+    }
+
+
+}
+class Classic extends Hardcover{
+    private int origPubYear;
+    private Author theAuthor;
+    private Publisher[] bookPublisher;
+
+    public Classic()
+    {
+        super();
+    }
+  
+    public void setOrigPubYear(int origPubYear)
+    {
+        this.origPubYear= origPubYear;
+    }
+    public int getOrigPubYear()
+    {
+        return this.origPubYear;
+    }
+    public void setTheAuthor(Author theAuthor)
+    {
+        this.theAuthor = theAuthor;
+    }
+    public Author getTheAuthor()
+    {
+        return this.theAuthor;
+    }
+    public void setBookPublisher(Publisher[] bookPublisher)
+    {
+        this.bookPublisher = bookPublisher;
+    } 
+    public Publisher[] getBookPublisher()
+    {
+        return this.bookPublisher;
+    }
+    public String createNotes()
+    {
+        return "Method createNotes called from Classic";
+    }
+
+}
+class Nonfiction extends Paperback{
+    private Category deweyClassification;
+    public Nonfiction()
+    {
+        super();
+    }
+    public void setDeweyClassification(Category deweyClassification)
+    {
+        this.deweyClassification = deweyClassification;
+    }
+    public Category getDeweyClassification()
+    {
+        return this.deweyClassification;
+    }
+
+
+}
+class Fiction extends Paperback{
+    public Fiction()
+    {
+        super();
+    }
+    public String coverArt()
+    {
+        return "Method coverArt called from Fiction";
+    }
+    public String genre()
+    {
+        return "Method genre called from Fiction";
+    }
+
+}
+
+class Anthology extends Fiction{
+    private Story[] story;
+    public Anthology()
+    {
+        super();
+    }
+    public setStory(Story[] story)
+    {
+        this.story = story;
+    }
+    public Story[] getStory()
+    {
+        return this.getStory();
+    }
+    public String plot()
+    {
+        return "Method plot called from Anthology";
+    }
+
+
+}
+class Novel extends Fiction{
+    private Author theAuthor;
+    private Series mySeries;
+    public Novel()
+    {
+        super();
+    }
+    public setTheAuthor(Author theAuthor)
+    {
+        this.theAuthor = theAuthor;
+    }
+    public Author getTheAuthor()
+    {
+        return this.theAuthor;
+    }
+    public String theme()
+    {
+        return "Method theme called from Novel";
+    }
+
+}
+class Story{
+    private Author theAuthor;
+    public Story()
+    {
+
+    }
+    public void setTheAuthor(Author theAuthor)
+    {
+        this.theAuthor = theAuthor;
+    }
+    public Author getTheAuthor()
+    {
+        return this.theAuthor;
+    }
+    public String plot()
+    {
+        return "Method plot called from Story";
+    }
+
+}
+class Publisher{
+    private String name;
+    private String address;
+    private Classic[] classicsCatalog;
+    public Publisher(String name, String address)
+    {
+        this.name = name;
+        this.address = address;
+
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+    public void getAddress()
+    {
+        return this.address;
+    }
+    public void setClassicsCatalog(Classic[] classicsCatalog)
+    {
+        this.classicsCatalog = classicsCatalog;
+    }
+    public Classic[] getClassicsCatalog()
+    {
+        return this.classicsCatalog;
+    }
+    public String printLetterHead()
+    {
+        return "Method printLetterHead called from Publisher";
+    }
+}
+class Author{
+    private String name;
+    private String address;
+
+}
+class Series{
+
+}
+class Contract{
+
+}
+class Category{
+    private Category subCategory;
+    private Category superCategory;
+    private String category;
+    public Category()
+    {
+
+    }
+    public void setSubCategory(Category subCategory)
+    {
+        this.subCategory = subCategory;
+    }
+    public Category getSubCategory()
+    {
+        return this.subCategory;
+    }
+    public void setSuperCategory(Category superCategory)
+    {
+        this.superCategory = superCategory;
+    }
+    public Category getSuperCategory()
+    {
+        return this.superCategory;
+    }
+    public void setCategory(String category)
+    {
+        this.category = category;
+    }
+    public String getCategory()
+    {
+        return this.category;
+    }
+    public String sort()
+    {
+        return "Method sort called from Category";
+    }
+
 }
